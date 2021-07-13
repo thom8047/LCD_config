113a22d0b22922a92345edeba480de4371289864
thom8047 <thom8047@bears.unco.edu>
2021-07-13 14:42:13 -0600

    updated again

--- display.py
+++ display.py
@@ -4,14 +4,10 @@ import time
 GPIO.setmode(GPIO.BOARD)  # set up BCM GPIO numbering  
 GPIO.setup(7, GPIO.OUT) # set GPIO 25 as input  
 
-while (True):
-    try:
-        GPIO.output(7, True)
-        time.sleep(1)
-        GPIO.output(7, False)
-        time.sleep(1)
-    except Exception as err:
-        print("\nEND SCRIPT\n")
-        break
+for i in range (50):
+    GPIO.output(7, True)
+    time.sleep(1)
+    GPIO.output(7, False)
+    time.sleep(1)
     
 GPIO.cleanup()
