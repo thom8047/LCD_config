db78cb3088b92bc621c94cd361b93facbb6127e8
thom8047 <thom8047@bears.unco.edu>
2021-07-13 11:26:28 -0600

    Update to try new gpio access

--- display.py
+++ display.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  # set up BCM GPIO numbering  
 GPIO.setup(9, GPIO.IN) # set GPIO 25 as input  
 
-if GPIO.input(25): # if port 25 == 1  
-    print("Port 25 is 1/GPIO.HIGH/True")
+if GPIO.input(9): # if port 25 == 1  
+    print(GPIO.input(9))
 
 GPIO.cleanup()
