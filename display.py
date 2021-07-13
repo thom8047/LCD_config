6be6d0e7d2f98b47212ed902340c231b1850ec0e
thom8047 <thom8047@bears.unco.edu>
2021-07-12 21:14:36 -0600

    update script

--- display.py
+++ display.py
@@ -1,10 +1,10 @@
 import os
 import glob
 import time
-import RPi
+import RPi.GPIO as GPIO
 from RPLCD import CharLCD
 
-RPi.GPIO.setmode(RPi.GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 
