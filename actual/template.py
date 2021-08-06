e9ef0fb6794d5451c1118c6f1cd7e63a22aa96f8
thom8047 <55425085+thom8047@users.noreply.github.com>
2021-08-05 22:32:42 -0600

    Creating a template for final project

---
+++ actual/template.py
@@ -0,0 +1,7 @@
+# uing these three packages as all the code I need
+
+import RPi.GPIO as gpio
+import gpiozero as gz
+from RPLCD.i2c import CharLCD as LCD
+
+# start here to get LCD, magnet, any leds, and all the different gpio's we might need
