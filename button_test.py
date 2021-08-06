ef936ab82df0f1b995e882eede210d013e81c7c9
thom8047 <55425085+thom8047@users.noreply.github.com>
2021-08-05 23:09:20 -0600

    Create button_test.py

---
+++ button_test.py
@@ -0,0 +1,35 @@
+import gpiozero as gz
+from RPLCD.i2c import CharLCD as LCD
+from time import sleep
+
+button = gz.Button(26)
+lcd = LCDLCD("PCF8574", 0x27)
+
+# init
+button.hold_time = 6
+lcd.backlight_enabled = False;
+
+def lcd_light(on):
+    lcd.backlight_enabled = on
+    
+# use when_help, when_pressed instead to capture buttons
+
+def main():
+    while True:
+        if (button.is_pressed):
+            lcd_light(True)
+            lcd.write_string("Button has been pressed")
+            sleep(5)
+            lcd.clear()
+            lcd_light(False)
+        if (button.is_held):
+            lcd_light(True)
+            lcd.write_string("Button has been help for 5sec")
+            sleep(5)
+            lcd.clear()
+            lcd_light(False)
+        sleep(1)
+    
+
+if __name__ == "__main__":
+    main()
